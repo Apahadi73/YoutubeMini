9e77dee8b36a755fcb3b4ebd93f2231d42727ce9
Amir <apahadi73@gmail.com>
2020-09-23 00:40:55 -0500

    Updates

---
+++ precache-manifest.72ba946ec6b13ba85dee47aa9c2ba2fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1afe2fed27396ed62f6ab0e9c7569d18",
+    "url": "/Apahadi73/YoutubeMini/index.html"
+  },
+  {
+    "revision": "9d52fd2027165247ce07",
+    "url": "/Apahadi73/YoutubeMini/static/css/main.8b471234.chunk.css"
+  },
+  {
+    "revision": "22da27bfaba8b0ffe1a5",
+    "url": "/Apahadi73/YoutubeMini/static/js/2.170fd691.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Apahadi73/YoutubeMini/static/js/2.170fd691.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d52fd2027165247ce07",
+    "url": "/Apahadi73/YoutubeMini/static/js/main.daa0dbdd.chunk.js"
+  },
+  {
+    "revision": "cd2399580d9211a8a846",
+    "url": "/Apahadi73/YoutubeMini/static/js/runtime-main.6db1b79c.js"
+  }
+]);
